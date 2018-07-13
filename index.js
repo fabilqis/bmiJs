73e211209cd6fb41a4bfa19ce79923560383cba6
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-13 15:17:52 +0700

    Debug BMI category function

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-function bmi(berat, tinggi){ 
-    return berat/(tinggi*tinggi);   
-}
-console.log(bmi(10, 1.63));
- 
-function bmi1(bmi){
-    if (bmi<=18){
-        return "Too skinny";
-    }
-    else if(bmi>=25){
-        return "Too fat";    
-    }
-    else{
-        return "normal";
-    } 
+function calculateBMI(berat, tinggi) {
+	return berat / (tinggi * tinggi);
 }
 
-console.log(bmi1());
+const bmiResult = calculateBMI(10, 1.63)
+
+console.log("bmi:", bmiResult);
+
+function categorizeBMI(bmi) {
+	if (bmi <= 18) {
+		return "Too skinny";
+	} else if (bmi >= 25) {
+		return "Too fat";
+	} else {
+		return "normal";
+	}
+}
+
+const bmiCategory = categorizeBMI(bmiResult)
+
+console.log("category:", bmiCategory);
